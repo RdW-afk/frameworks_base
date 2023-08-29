1ee203370c59dd94e31fe13bb0a1be74679b633d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-29 13:18:21 +0800

    core: Always clear the cache when the framework resources packages is changed
    Change-Id: Ifdabf3907c421bff76631ffd614a467a15799f07
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -143,7 +143,7 @@ public class ZygoteInit {
         cacheNonBootClasspathClassLoaders();
         bootTimingsTraceLog.traceEnd(); // CacheNonBootClasspathClassLoaders
         bootTimingsTraceLog.traceBegin("PreloadResources");
-        preloadResources();
+        Resources.resetPreloadDrawableStateCache();
         bootTimingsTraceLog.traceEnd(); // PreloadResources
         Trace.traceBegin(Trace.TRACE_TAG_DALVIK, "PreloadAppProcessHALs");
         nativePreloadAppProcessHALs();
